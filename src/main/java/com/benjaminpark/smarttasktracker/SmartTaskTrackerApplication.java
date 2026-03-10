8a858281b85d449d86dfc4f53b5826ad5a94e220
Benjamin Park <bentompar@gmail.com>
2026-03-10 19:40:43 +0100

    Initial commit

---
+++ src/main/java/com/benjaminpark/smarttasktracker/SmartTaskTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.benjaminpark.smarttasktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTaskTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartTaskTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/benjaminpark/smarttasktracker/SmartTaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benjaminpark.smarttasktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartTaskTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
