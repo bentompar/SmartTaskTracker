efb25a20fa230dbc5740a7862a06aec64c360800
Benjamin Park <bentompar@gmail.com>
2026-03-23 19:21:33 +0100

    feat: add CustomUserDetails and CustomUserDetailsService

---
+++ src/main/java/com/benjaminpark/smarttasktracker/security/CustomUserDetails.java
@@ -0,0 +1,64 @@
+package com.benjaminpark.smarttasktracker.security;
+
+import com.benjaminpark.smarttasktracker.model.Client;
+import com.benjaminpark.smarttasktracker.model.enums.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetails {
+
+    private String clientId;
+    private String username;
+    private String password;
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(role.toString()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public CustomUserDetails(Client client) {
+        this.clientId = client.getClientId();
+        this.username = client.getUsername();
+        this.password = client.getPassword();
+        this.role = client.getRole();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/benjaminpark/smarttasktracker/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.benjaminpark.smarttasktracker.security;
+
+import com.benjaminpark.smarttasktracker.model.Client;
+import com.benjaminpark.smarttasktracker.repository.ClientRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final ClientRepository clientRepository;
+
+    public CustomUserDetailsService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Client client = clientRepository.findByUsername(username);
+        if (client == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new CustomUserDetails(client);
+    }
+}
